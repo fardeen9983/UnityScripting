cadd3281375f622b6883f197886ccc47c3d7340e
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-14 00:12:22 +0530

    Scene4 Transform and rotate

--- Assets/TranslateRotate.cs
+++ Assets/TranslateRotate.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TranslateRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
     }
 }
