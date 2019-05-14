303f1fc9bc13ba083c773edcc5265099b145e81a
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-14 12:53:28 +0530

    OnMouseDown example Scene8

---
+++ Assets/MouseClicks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClicks : MonoBehaviour
+{
+    private int counter = 0;
+    private Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        switch (counter)
+        {
+            case 0: renderer.material.color = Color.blue; break;
+            case 1: renderer.material.color = Color.red; break;
+            case 2: renderer.material.color = Color.green; break;
+        }
+        counter++;
+        counter = counter % 3;
+    }
+}
