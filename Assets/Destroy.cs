ef6c9663b1bffbb04ccf897c474e6a4e3c088af0
Fardeen Khan <khanfardeen9983@gmail.com>
2019-05-14 07:02:46 +0530

    Scene6 Destroy function

---
+++ Assets/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    //Reference to another GameObject
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Delete))
+            Destroy(gameObject);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Destroy(gameObject.GetComponent<Light>(),3f);
+    }
+}
